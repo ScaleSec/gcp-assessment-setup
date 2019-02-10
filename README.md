f06862a6e66afc7bab7ee622fddf96fc1c33eee3
aaron <aaron@scalesec.com>
2019-02-10 15:17:21 -0800

    1st push

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # gcp-assessment-setup
 Give ScaleSec limited access to your GCP organization for a security assessment.
+
+## Prerequisites
+You'll need permissions to modify and apply IAM roles to your Google Cloud organization.
+
+## System requirements
+The following items are required for a successful setup.
+
+1. A Linux or Mac system to run the script (sorry, not tested in Windows)
+1. The [gcloud](https://cloud.google.com/sdk/) SDK CLI
+1. The `jq` CLI utility for your chosen platform
+
+## Setup instructions
+
+1. Open your Google Cloud [console](https://console.cloud.google.com).
+2. Open Cloud Shell
+3. Clone this repositry and switch to its directory:
+
+```
+git clone https://github.com/ScaleSec/gcp-assessment-setup.git
+cd gcp-security-assessment
+```
+
+4. Edit the `manage_security_assessment_role.sh` and set variables:
+
+```
+ORG_NAME="example.com"
+ROLE_ID="scalesec_assessment"
+YAML_PATH="./security_assessment_role.yaml"
+SERVICE_ACCOUNT="scalesec-security-assessment@scalesec-dev.iam.gserviceaccount.com"
+```
+
+5. Run the script:
+
+```
+bash security_assessment_role.yaml create
+```
+
+6. 
---
+++ manage_security_assessment_role.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# Manages an organization custom role with minimum permissions for ScaleSec to perform GCP security assessments
+#
+# requires:
+#   IAM permissions on your Google Cloud organization
+#   gcloud CLI https://cloud.google.com/sdk/
+#   jq
+
+
+# variables
+ORG_NAME="scalesec.com"
+ROLE_ID="scalesec_assessment"
+YAML_PATH="./security_assessment_role.yaml"
+SERVICE_ACCOUNT="scalesec-security-assessment@scalesec-dev.iam.gserviceaccount.com"
+
+get_org_id ()
+{
+    ORG_ID=`gcloud organizations list --filter="displayName~'^${ORG_NAME}'" --format=yaml | grep organizations | awk -F/ '{print $2}'`
+    if [ -z ${ORG_ID} ]; then
+        echo "FATAL: Cannot describe organization \"${ORG_NAME}\". Check permissions."
+        exit 1
+    else
+        echo "Organization \"${ORG_NAME}\" has ID \"${ORG_ID}\"."
+    fi
+}
+
+create ()
+{
+    # make sure role does NOT exist so we can create it
+    if [ -z "${describe_role}" ]; then
+        gcloud iam roles create "${ROLE_ID}" --organization "${ORG_ID}" --file "${YAML_PATH}"
+        create_iam_binding
+    elif [ "${deleted}" == "true" ]; then
+        undelete
+    else
+        echo "Info: role \"${ROLE_ID}\" already exists and has not been deleted. Switching to \"replace\"..."
+        replace
+    fi
+}
+
+# since we don't include an "etag" field, the update command will replace existing role
+replace ()
+{
+    # make sure role exists so we can replace it
+    if [ -z "${describe_role}" ]; then
+        echo "FATAL: role \"${ROLE_ID}\" does not exist."
+        exit 1
+    elif [ "${deleted}" == "true" ]; then
+        undelete
+    else
+        echo "Replacing existing role...."
+        gcloud iam roles update ${ROLE_ID} --organization ${ORG_ID} --file ${YAML_PATH}
+    fi
+}
+
+undelete ()
+{
+    echo "Info: role \"${ROLE_ID}\" exists but has been deleted. Switching to \"undelete\"..."
+    gcloud iam roles undelete "${ROLE_ID}" --organization "${ORG_ID}"
+    create_iam_binding
+}
+
+delete ()
+{
+    # make sure role exists so we can delete it
+    if [ -z "${describe_role}" ]; then
+        echo "FATAL: role \"${ROLE_ID}\" does not exist."
+        exit 1
+    elif [ "${deleted}" == "true" ]; then
+        echo "Info: role \"${ROLE_ID}\" has already been scheduled for deletion."
+    else
+        echo "Scheduling role for deletion..."
+        remove_iam_binding
+        gcloud iam roles delete ${ROLE_ID} --organization ${ORG_ID}
+    fi
+    echo "
+    Note: After 7 days, the role is scheduled for permanent deletion. This process takes 30 days. 
+    During the 30 day window, the role and all associated bindings are permanently removed, and 
+    you cannot create a new role with the same role ID.
+    Reference: https://cloud.google.com/iam/docs/creating-custom-roles#deleting_a_custom_role
+    "
+}
+
+create_iam_binding () 
+{
+    gcloud organizations add-iam-policy-binding "${ORG_ID}" --member=serviceAccount:"${SERVICE_ACCOUNT}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}"
+}
+
+remove_iam_binding ()
+{
+    gcloud organizations remove-iam-policy-binding "${ORG_ID}" --member=serviceAccount:"${SERVICE_ACCOUNT}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}"    
+}
+
+# get the organization ID
+get_org_id
+
+# look for conflicts
+describe_role=`gcloud iam roles describe "${ROLE_ID}" --organization "${ORG_ID}"`
+deleted=`gcloud iam roles describe --organization "${ORG_ID}" "${ROLE_ID}" --format json | jq -r '.deleted'`
+
+# perform action depending on argument
+case "${1}" in
+    create)
+        create
+    ;;
+    replace|update)
+        replace
+    ;;
+    destroy|delete|remove)
+        delete
+    ;;
+    *)
+        echo $"Usage: $0 {create|replace|destroy}"
+        exit 1
+    ;;
+esac
