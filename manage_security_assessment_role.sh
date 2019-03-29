bb6e29da414640b52fd8a29b456d06a1f6de3349
Chris Leibl <christopher@Chriss-MacBook-Pro.local>
2019-03-29 15:16:14 -0400

    revert

--- manage_security_assessment_role.sh
+++ manage_security_assessment_role.sh
@@ -9,8 +9,8 @@
 
 
 # variables
-ORG_NAME="gamine-technologies.com"
-ROLE_ID="scalesec_assessment_4"
+ORG_NAME="example.com"
+ROLE_ID="scalesec_assessment"
 YAML_PATH="./security_assessment_role.yaml"
 SERVICE_ACCOUNT="scalesec-security-assessment@scalesec-dev.iam.gserviceaccount.com"
 
