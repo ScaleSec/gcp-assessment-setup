a6e7616161b5b01720ee854c584dc1a72ca79c22
Chris Leibl <christopher@Chriss-MacBook-Pro.local>
2020-09-25 12:52:45 -0400

    add --condition=None and update deleted commannd for consistency

--- manage_security_assessment_role.sh
+++ manage_security_assessment_role.sh
@@ -9,7 +9,7 @@
 
 
 # variables
-ORG_NAME="example.com"
+ORG_NAME="gamine-technologies.com"
 ROLE_ID="scalesec_assessment"
 YAML_PATH="./security_assessment_role.yaml"
 GROUP="scalesec-assessment@scalesec.com"
@@ -85,12 +85,12 @@ delete ()
 
 create_iam_binding () 
 {
-    gcloud organizations add-iam-policy-binding "${ORG_ID}" --member=group:"${GROUP}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}"
+    gcloud organizations add-iam-policy-binding "${ORG_ID}" --member=group:"${GROUP}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}" --condition=None
 }
 
 remove_iam_binding ()
 {
-    gcloud organizations remove-iam-policy-binding "${ORG_ID}" --member=group:"${GROUP}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}"     
+    gcloud organizations remove-iam-policy-binding "${ORG_ID}" --member=group:"${GROUP}" --role organizations/"${ORG_ID}"/roles/"${ROLE_ID}" --condition=None    
 }
 
 # get the organization ID
@@ -98,7 +98,7 @@ get_org_id
 
 # look for conflicts
 describe_role=`gcloud iam roles describe "${ROLE_ID}" --organization "${ORG_ID}"`
-deleted=`gcloud iam roles describe --organization "${ORG_ID}" "${ROLE_ID}" --format json | jq -r '.deleted'`
+deleted=`gcloud iam roles describe "${ROLE_ID}" --organization "${ORG_ID}" --format json | jq -r '.deleted'`
 
 # perform action depending on argument
 case "${1}" in
